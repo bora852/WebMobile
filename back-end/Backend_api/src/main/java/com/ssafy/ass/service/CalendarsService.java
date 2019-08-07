e7e848291cc9842e35f8336a888c1525f91cbee5
kimhyebin <rlagpqls123@naver.com>
2019-08-07 15:48:19 +0900

    back-end 구현중

---
+++ back-end/Backend_api/src/main/java/com/ssafy/ass/service/CalendarsService.java
@@ -0,0 +1,37 @@
+package com.ssafy.ass.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ssafy.ass.dao.CalendarsMapper;
+import com.ssafy.ass.dto.CalendarsDto;
+
+@Service
+public class CalendarsService {
+
+	@Autowired
+	private CalendarsMapper calendarsMapper;
+	
+	//CRUD
+	public List<CalendarsDto> searchAllCalen(){
+		return calendarsMapper.searchAllCalen();
+	};
+	
+	public CalendarsDto searchOnceCalen(int idx){
+		return calendarsMapper.searchOnceCalen(idx);
+	};
+	
+	public int deleteCalen(int idx){
+		return calendarsMapper.deleteCalen(idx);
+	};
+	
+	public int updateCalen(CalendarsDto calen){
+		return calendarsMapper.updateCalen(calen);
+	};
+	
+	public int insertCalen(CalendarsDto calen){
+		return calendarsMapper.insertCalen(calen);
+	};
+}
